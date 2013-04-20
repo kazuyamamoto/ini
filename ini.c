abd78d23637ecde84da4be45d56b4541bb0dd044
Kazuhiro Yamamoto <kazu@rao.co.jp>
2013-04-20 14:12:32 +0900

    Add test case.

--- ini.c
+++ ini.c
@@ -6,6 +6,7 @@
 #include "ini.h"
 #include "futil.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 struct Ini {
 };
@@ -16,10 +17,28 @@ Ini* ini_new()
 	if (ini == NULL) {
 		return NULL;
 	}
-	return NULL;
+
+	return ini;
+}
+
+Ini* ini_read(FILE* file)
+{
+	Ini* ini = ini_new();
+	if (ini == NULL) {
+		return NULL;
+	}
+
+	// TODO: file read.
+
+	return ini;
 }
 
 void ini_delete(Ini* ini)
 {
 	free(ini);
+}
+
+const char* ini_get(const Ini* ini, const char* section, const char* key)
+{
+	return NULL;
 }
--- test_ini.c
+++ test_ini.c
@@ -1,5 +1,6 @@
 #include "PCUnit/PCUnit.h"
 #include "ini.h"
+#include <stdio.h>
 
 static int initialize(void)
 {
@@ -22,9 +23,21 @@ static int teardown(void)
 }
 
 
-static void test_TODO(void)
+static void test_ini_get(void)
 {
-	Ini* ini = ini_new();
+	Ini* ini;
+	FILE *file;
+	const char* value;
+
+	file = fopen("sample1.ini", "r");
+	PCU_ASSERT_PTR_NOT_NULL(file);
+
+	ini = ini_read(file);
+	fclose(file);
+	PCU_ASSERT_PTR_NOT_NULL(ini);
+
+	value = ini_get(ini, "abc", "def");
+	PCU_ASSERT_STRING_EQUAL(value, "ghi");
 
 	ini_delete(ini);
 }
@@ -33,7 +46,7 @@ static void test_TODO(void)
 PCU_Suite *test_ini_suite(void)
 {
 	static PCU_Test tests[] = {
-		PCU_TEST(test_TODO),
+		PCU_TEST(test_ini_get),
 	};
 	static PCU_Suite suite = {
 		"ini_test", tests, sizeof tests / sizeof tests[0], setup, teardown, initialize, cleanup
