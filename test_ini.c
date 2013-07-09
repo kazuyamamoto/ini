f832646e063ff01987bac3d5f5964aad9f6525fe
Kazuhiro Yamamoto <kazu@rao.co.jp>
2013-07-09 21:54:39 +0900

    Add test case.

--- test_ini.c
+++ test_ini.c
@@ -66,12 +66,35 @@ static void test_ini_get_two_sections(void)
 static void test_ini_get_ignore_empty_line(void)
 {
 	Ini *ini;
-	const char *value;
+	const char *value1, *value2;
 
-	ini = ini_parse("\n[section]\n\nname=value\n\n");
-	value = ini_get(ini, "section", "name");
-	PCU_ASSERT_PTR_NOT_NULL(value);
-	PCU_ASSERT_STRING_EQUAL("value", value);
+	ini = ini_parse("\n[section]\n\nname1=value1\n\nname2=value2");
+
+	value1 = ini_get(ini, "section", "name1");
+	PCU_ASSERT_PTR_NOT_NULL(value1);
+	PCU_ASSERT_STRING_EQUAL("value1", value1);
+
+	value2 = ini_get(ini, "section", "name2");
+	PCU_ASSERT_PTR_NOT_NULL(value2);
+	PCU_ASSERT_STRING_EQUAL("value2", value2);
+
+	ini_delete(ini);
+}
+
+static void test_ini_get_ignore_comment_line(void)
+{
+	Ini *ini;
+	const char *value1, *value2;
+
+	ini = ini_parse(";comment1\n[section]\n;comment2\nname1=value1\n;comment3\n");
+
+	value1 = ini_get(ini, "section", "name1");
+	PCU_ASSERT_PTR_NOT_NULL(value1);
+	PCU_ASSERT_STRING_EQUAL("value1", value1);
+
+	value2 = ini_get(ini, "section", "name2");
+	PCU_ASSERT_PTR_NOT_NULL(value2);
+	PCU_ASSERT_STRING_EQUAL("value2", value2);
 
 	ini_delete(ini);
 }
@@ -83,6 +106,7 @@ PCU_Suite *test_ini_suite(void)
 		PCU_TEST(test_ini_get_two_keys),
 		PCU_TEST(test_ini_get_two_sections),
 		PCU_TEST(test_ini_get_ignore_empty_line),
+		PCU_TEST(test_ini_get_ignore_comment_line),
 	};
 	static PCU_Suite suite = {
 		"test_ini", tests, sizeof tests / sizeof tests[0]
