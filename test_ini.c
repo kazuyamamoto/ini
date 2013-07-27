f07efef4ce81687e512d582ce0924feeb8206013
Kazuhiro Yamamoto <kazu@rao.co.jp>
2013-07-28 07:58:31 +0900

    Add ini test cases for new-line character CR.

--- test_ini.c
+++ test_ini.c
@@ -22,12 +22,46 @@ static void test_ini_get(void)
 	ini_delete(ini);
 }
 
+static void test_ini_get_cr(void)
+{
+	Ini *ini;
+	const char *value;
+
+	ini = ini_parse("[section]\rname=value");
+	PCU_ASSERT_PTR_NOT_NULL(ini);
+
+	value = ini_get(ini, "section", "name");
+	PCU_ASSERT_PTR_NOT_NULL(value);
+	PCU_ASSERT_STRING_EQUAL("value", value);
+
+	ini_delete(ini);
+}
+
 static void test_ini_get_two_keys(void)
 {
 	Ini *ini;
 	const char *value1, *value2;
 
 	ini = ini_parse("[section]\nname1=value1\nname2=value2");
+	PCU_ASSERT_PTR_NOT_NULL(ini);
+
+	value1 = ini_get(ini, "section", "name1");
+	PCU_ASSERT_PTR_NOT_NULL(value1);
+	PCU_ASSERT_STRING_EQUAL("value1", value1);
+
+	value2 = ini_get(ini, "section", "name2");
+	PCU_ASSERT_PTR_NOT_NULL(value2);
+	PCU_ASSERT_STRING_EQUAL("value2", value2);
+
+	ini_delete(ini);
+}
+
+static void test_ini_get_two_keys_cr(void)
+{
+	Ini *ini;
+	const char *value1, *value2;
+
+	ini = ini_parse("[section]\rname1=value1\rname2=value2");
 	PCU_ASSERT_PTR_NOT_NULL(ini);
 
 	value1 = ini_get(ini, "section", "name1");
@@ -81,6 +115,24 @@ static void test_ini_get_ignore_empty_line(void)
 	ini_delete(ini);
 }
 
+static void test_ini_get_ignore_empty_line_cr(void)
+{
+	Ini *ini;
+	const char *value1, *value2;
+
+	ini = ini_parse("\r[section]\r\rname1=value1\r\rname2=value2");
+
+	value1 = ini_get(ini, "section", "name1");
+	PCU_ASSERT_PTR_NOT_NULL(value1);
+	PCU_ASSERT_STRING_EQUAL("value1", value1);
+
+	value2 = ini_get(ini, "section", "name2");
+	PCU_ASSERT_PTR_NOT_NULL(value2);
+	PCU_ASSERT_STRING_EQUAL("value2", value2);
+
+	ini_delete(ini);
+}
+
 static void test_ini_get_ignore_spacetab_line(void)
 {
 	Ini *ini;
@@ -99,6 +151,23 @@ static void test_ini_get_ignore_spacetab_line(void)
 	ini_delete(ini);
 }
 
+static void test_ini_get_ignore_spacetab_line_cr(void)
+{
+	Ini *ini;
+	const char *value1, *value2;
+
+	ini = ini_parse(" \t\r[section]\r \t\rname1=value1\r \t\rname2=value2 \t\r");
+
+	value1 = ini_get(ini, "section", "name1");
+	PCU_ASSERT_PTR_NOT_NULL(value1);
+	PCU_ASSERT_STRING_EQUAL("value1", value1);
+
+	value2 = ini_get(ini, "section", "name2");
+	PCU_ASSERT_PTR_NOT_NULL(value2);
+	PCU_ASSERT_STRING_EQUAL("value2", value2);
+
+	ini_delete(ini);
+}
 
 static void test_ini_get_ignore_comment_line(void)
 {
@@ -106,6 +175,24 @@ static void test_ini_get_ignore_comment_line(void)
 	const char *value1, *value2;
 
 	ini = ini_parse(" ;comment1\n[section]\n\t;comment2\nname1=value1\n;comment3\nname2=value2\n;comment4");
+
+	value1 = ini_get(ini, "section", "name1");
+	PCU_ASSERT_PTR_NOT_NULL(value1);
+	PCU_ASSERT_STRING_EQUAL("value1", value1);
+
+	value2 = ini_get(ini, "section", "name2");
+	PCU_ASSERT_PTR_NOT_NULL(value2);
+	PCU_ASSERT_STRING_EQUAL("value2", value2);
+
+	ini_delete(ini);
+}
+
+static void test_ini_get_ignore_comment_line_cr(void)
+{
+	Ini *ini;
+	const char *value1, *value2;
+
+	ini = ini_parse(" ;comment1\r[section]\r\t;comment2\rname1=value1\r;comment3\rname2=value2\r;comment4");
 
 	value1 = ini_get(ini, "section", "name1");
 	PCU_ASSERT_PTR_NOT_NULL(value1);
@@ -151,6 +238,23 @@ static void test_ini_get_unknown_line_after_first_section(void)
 	ini_delete(ini);
 }
 
+static void test_ini_get_unknown_line_after_first_section_cr(void)
+{
+	Ini *ini;
+	const char *value1, *value2;
+
+	ini = ini_parse("[section]\rname1=value1\rabc\rname2=value2");
+
+	value1 = ini_get(ini, "section", "name1");
+	PCU_ASSERT_PTR_NOT_NULL(value1);
+	PCU_ASSERT_STRING_EQUAL("value1", value1);
+
+	value2 = ini_get(ini, "section", "name2");
+	PCU_ASSERT_PTR_NULL(value2);
+
+	ini_delete(ini);
+}
+
 PCU_Suite *test_ini_suite(void)
 {
 	static PCU_Test tests[] = {
@@ -162,6 +266,12 @@ PCU_Suite *test_ini_suite(void)
 		PCU_TEST(test_ini_get_ignore_spacetab_line),
 		PCU_TEST(test_ini_get_unknown_line_before_first_section),
 		PCU_TEST(test_ini_get_unknown_line_after_first_section),
+		PCU_TEST(test_ini_get_cr),
+		PCU_TEST(test_ini_get_two_keys_cr),
+		PCU_TEST(test_ini_get_ignore_empty_line_cr),
+		PCU_TEST(test_ini_get_ignore_spacetab_line_cr),
+		PCU_TEST(test_ini_get_ignore_comment_line_cr),
+		PCU_TEST(test_ini_get_unknown_line_after_first_section_cr),
 	};
 	static PCU_Suite suite = {
 		"test_ini", tests, sizeof tests / sizeof tests[0]
